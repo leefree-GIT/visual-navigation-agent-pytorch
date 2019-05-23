bb61f2d4591508c69b81e2a579b455ced1e7b388
Raphael Druon <raphdruon@gmail.com>
2019-05-23 09:47:13 +0900

    (feat) User fixed thread number

--- agent/training.py
+++ agent/training.py
@@ -11,14 +11,8 @@ import os
 import threading
 from contextlib import suppress
 import re
-
-TOTAL_PROCESSED_FRAMES = 20 * 10**6 # 10 million frames
-TASK_LIST = {
-  'bathroom_02': ['26', '37', '43', '53', '69'],
-  'bedroom_04': ['134', '264', '320', '384', '387'],
-  'kitchen_02': ['90', '136', '157', '207', '329'],
-  'living_room_08': ['92', '135', '193', '228', '254']
-}
+from agent.constants import TOTAL_PROCESSED_FRAMES
+from agent.constants import TASK_LIST
 
 class TrainingSaver:
     def __init__(self, shared_network, scene_networks, optimizer, config):
@@ -142,6 +136,7 @@ class Training:
         self.tasks = config.get('tasks', TASK_LIST)
         self.checkpoint_path = config.get('checkpoint_path', 'model/checkpoint-{checkpoint}.pth')
         self.max_t = config.get('max_t', 5)
+        self.num_thread = config.get('num_thread', 1)
         self.total_epochs = TOTAL_PROCESSED_FRAMES // self.max_t
         self.initialize()
 
@@ -222,9 +217,20 @@ class Training:
                 saver = self.saver,
                 max_t = self.max_t,
                 terminal_state_id = target,
+                max_step = TOTAL_PROCESSED_FRAMES,
                 **self.config)
 
-        self.threads = [_createThread(i, task) for i, task in enumerate(branches)]
+        num_scene_task =  len(branches)
+        if self.num_thread < num_scene_task:
+            self.num_thread = num_scene_task
+            print('ERROR: num_thread must be higher than ', num_scene_task)
+
+        self.threads = []
+        for i in range(self.num_thread):
+            self.threads.append(_createThread(i, branches[i%num_scene_task]))
+
+        
+        # self.threads = [_createThread(i, task) for i, task in enumerate(branches)]
         
         try:
             for thread in self.threads:
@@ -232,10 +238,13 @@ class Training:
 
             for thread in self.threads:
                 thread.join()
+            self.saver.save()
         except KeyboardInterrupt:
             # we will save the training
             print('Saving training session')
             self.saver.save()
+            for thread in self.threads:
+                thread.stop()
         
 
     def _init_logger(self):
