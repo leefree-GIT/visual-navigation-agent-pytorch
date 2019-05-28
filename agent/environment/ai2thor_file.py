05a86cef23fcc5b6483b1d13f97917397cca9e86
Raphael Druon <raphdruon@gmail.com>
2019-05-28 09:27:41 +0900

    (feat) Stat working on real AI2THOR env

---
+++ agent/environment/ai2thor_file.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+import sys
+import h5py
+import json
+import numpy as np
+import random
+import skimage.io
+from skimage.transform import resize
+from agent.environment.environment import Environment
+from torchvision import transforms
+import torchvision.transforms.functional as F
+import torch
+
+class THORDiscreteEnvironment(Environment):
+    def __init__(self, 
+            scene_name = 'bedroom_04',
+            resnet_trained = None,
+            random_start = True,
+            n_feat_per_location = 1,
+            history_length : int = 4,
+            screen_width = 224,
+            screen_height = 224,
+            terminal_state_id = 0,
+            h5_file_path = None,
+            obs_preloaded = None,
+            **kwargs):
+        super(THORDiscreteEnvironment, self).__init__()
+
+
+
+        if h5_file_path is None:
+            h5_file_path = f"/app/data/{scene_name}.h5"
+        elif callable(h5_file_path):
+            h5_file_path = h5_file_path(scene_name)
+            
+        if resnet_trained is not None:
+            self.resnet_trained = resnet_trained
+            self.use_resnet = True
+        else:
+            self.use_resnet = False
+
+        self.terminal_state_id = terminal_state_id
+
+        self.h5_file = h5py.File(h5_file_path, 'r')
+
+        self.n_feat_per_location = n_feat_per_location
+
+        self.locations = self.h5_file['location'][()]
+        self.rotations = self.h5_file['rotation'][()]
+        self.resized_obs_tens = obs_preloaded
+
+        self.history_length = history_length
+
+        self.n_locations = self.locations.shape[0]
+
+        self.terminals = np.zeros(self.n_locations)
+        self.terminals[terminal_state_id] = 1
+        self.terminal_states, = np.where(self.terminals)
+
+        self.transition_graph = self.h5_file['graph'][()]
+
+        self.shortest_path_distances = self.h5_file['shortest_path_distance'][()]
+
+        self.normalize = transforms.Compose([
+                transforms.Resize((224,224)), 
+                transforms.ToTensor(), 
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+        self.s_target = self._tiled_state(self.terminal_state_id)
+        self.time = 0
+
+    def reset(self):
+        # randomize initial state
+        while True:
+            k = random.randrange(self.n_locations)
+            min_d = np.inf
+
+            # check if target is reachable
+            for t_state in self.terminal_states:
+                dist = self.shortest_path_distances[k][t_state]
+                min_d = min(min_d, dist)
+
+            # min_d = 0  if k is a terminal state
+            # min_d = -1 if no terminal state is reachable from k
+            if min_d > 0: break
+        
+        # reset parameters
+        self.current_state_id = k # TODO: k
+        self.start_state_id = k
+        self.s_t = self._tiled_state(self.current_state_id)
+
+        self.collided = False
+        self.terminal = False
+        self.time = 0
+
+    def step(self, action):
+        assert not self.terminal, 'step() called in terminal state'
+        k = self.current_state_id
+        if self.transition_graph[k][action] != -1:
+            self.current_state_id = self.transition_graph[k][action]
+            if self.terminals[self.current_state_id]:
+                self.terminal = True
+                self.collided = False
+            else:
+                self.terminal = False
+                self.collided = False
+        else:
+            self.terminal = False
+            self.collided = True
+
+        self.s_t = np.append(self.s_t[:,1:], self._get_state(self.current_state_id), axis=1)
+        self.time = self.time + 1
+
+    def _get_state(self, state_id):
+        # read from hdf5 cache
+        k = random.randrange(self.n_feat_per_location)
+        if not self.use_resnet:
+            return self.h5_file['resnet_feature'][state_id][k][:,np.newaxis]
+        else:
+            input_tens = self.resized_obs_tens[state_id]
+            input_tens = input_tens.to(next(self.resnet_trained.parameters()).device)
+            input_tens = input_tens.unsqueeze(0)
+            res = self.resnet_trained((input_tens,))
+            return res.permute(1,0).cpu()
+
+    def _tiled_state(self, state_id):
+        f = self._get_state(state_id)
+        return np.tile(f, (1, self.history_length))
+
+    def _calculate_reward(self, terminal, collided):
+        # positive reward upon task completion
+        if terminal: return 10.0
+        # time penalty or collision penalty
+        return -0.1 if collided else -0.01
+
+    @property
+    def reward(self):
+        return self._calculate_reward(self.is_terminal, self.collided)
+
+    @property
+    def is_terminal(self):
+        return self.terminal or self.time >= 5e3
+
+    @property
+    def observation(self):
+        return self.h5_file['observation'][self.current_state_id]
+
+    def render(self, mode):
+        assert mode == 'resnet_features'
+        return self.s_t
+
+    def render_target(self, mode):
+        assert mode == 'resnet_features'
+        return self.s_target
+
+    @property
+    def actions(self):
+        return ["MoveForward", "RotateRight", "RotateLeft", "MoveBackward"]
+
+    @property
+    def shortest_path_distance_start(self):
+        return self.shortest_path_distances[self.start_state_id][self.terminal_state_id]
---
+++ agent/environment/ai2thor_real.py
@@ -0,0 +1,73 @@
+import time
+from environment import Environment
+import numpy as np
+
+class AI2ThorEnvironment(Environment):
+    '''
+    name:
+        Can be one of those:
+            # Kitchens:       FloorPlan1 - FloorPlan30
+            # Living rooms:   FloorPlan201 - FloorPlan230
+            # Bedrooms:       FloorPlan301 - FloorPlan330
+            # Bathrooms:      FloorPLan401 - FloorPlan430
+            # FloorPlan1
+    '''
+    def __init__(self, name = "FloorPlan1", grid_size = 0.25):
+        import ai2thor.controller
+        self.name = name
+        self.grid_size = grid_size
+        self.controller = ai2thor.controller.Controller()
+        self.state = None
+
+    def start(self):
+        self.controller.start() 
+        self.reset()
+
+    def reset(self):
+        self.controller.reset(self.name)
+
+        # gridSize specifies the coarseness of the grid that the agent navigates on
+        self.state = self.controller.step(dict(action='Initialize', gridSize=self.grid_size, renderObjectImage=True))
+        self.s_t = self._tiled_state(self.current_state_id)
+
+    def _tiled_state(self, state_id):
+        f = self._get_state(state_id)
+        return np.tile(f, (1, self.history_length))
+    
+    def _get_state(self, state_id):
+        input_tens = self.resized_obs_tens[state_id]
+        input_tens = input_tens.to(next(self.resnet_trained.parameters()).device)
+        input_tens = input_tens.unsqueeze(0)
+        res = self.resnet_trained((input_tens,))
+        return res.permute(1,0).cpu()
+
+    def step(self, action='MoveAhead'):
+        self.state = self.controller.step(dict(action=action))
+
+    def render(self, mode = "rgb_array"):
+        if mode == "rgb_array":
+            return self.state.frame
+
+    def render_target(self, mode = "rgb_array"):
+        if mode == "rgb_array":
+            return self.state
+
+    @property
+    def boudingbox(self):
+        return self.state.instance_detections2D
+        
+    @property
+    def actions(self):
+        return ["MoveAhead", "RotateRight", "RotateLeft", "MoveBackward"]
+
+def make(name):
+    if name == "unity":
+        return AI2ThorEnvironment()
+
+if __name__ == '__main__':
+    AI2ThorEnv = make('unity')
+    AI2ThorEnv.start()
+    while True:
+        AI2ThorEnv.step(AI2ThorEnv.actions[0])
+        print(AI2ThorEnv.boudingbox)
+        time.sleep(1)
