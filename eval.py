e22498ff4266ce99a01660d9ef1a2304854522f0
Raphael Druon <raphdruon@gmail.com>
2019-06-26 10:21:15 +0900

    (fix) Eval all checkpoints and store the output

--- eval.py
+++ eval.py
@@ -8,23 +8,6 @@ import torch
 from agent.evaluation import Evaluation
 from agent.utils import populate_config
 
-
-class Logger(object):
-    def __init__(self, path="logfile.log"):
-        self.terminal = sys.stdout
-        self.log = open(path, "w")
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-    def flush(self):
-        # this flush method is needed for python 3 compatibility.
-        # this handles the flush command by doing nothing.
-        # you might want to specify some extra behavior here.
-        pass
-
-
 if __name__ == '__main__':
     # mp.set_start_method('spawn')
     argparse.ArgumentParser(description="")
@@ -32,8 +15,6 @@ if __name__ == '__main__':
     parser.add_argument('--h5_file_path', type=str,
                         default='/app/data/{scene}_keras.h5')
     parser.add_argument('--checkpoint_path', type=str, default=None)
-    parser.add_argument('--csv_file', type=str, default=None)
-    parser.add_argument('--log_arg', type=int, default=0)
     parser.add_argument('--show', action='store_true')
 
     # Use experiment.json
@@ -46,8 +27,5 @@ if __name__ == '__main__':
     else:
         args = populate_config(args, mode='eval')
 
-    sys.stdout = Logger(args['base_path'] + 'eval' +
-                        str(args['log_arg']) + '.log')
-
-    t = Evaluation.load_checkpoint(args)
+    t = Evaluation.load_checkpoints(args)
     t.run(args['show'])
